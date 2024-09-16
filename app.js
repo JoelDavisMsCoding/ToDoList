e1b0860b46279c89b1d65f3ee9bdb7f30406ca02
JoelDavisMsCoding <166549396+JoelDavisMsCoding@users.noreply.github.com>
2024-09-16 00:43:07 -0500

    Finished and fully functioning

--- app.js
+++ app.js
@@ -1,61 +1,67 @@
-let inputInfo = document.getElementById("userInput");
-console.log(inputInfo);
-let addTaskButton = document.getElementById("addTask");
-console.log(addTaskButton);
-let displayCheckBox = document.querySelector(".displayCheckBox");
-let holdCheckBox = document.createElement("p");
-holdCheckBox.classList.add(".d-inline-block")
-let ulTag = document.createElement("ul");
-ulTag.classList.add(".d-flex");
-ulTag.style.listStyle = "none";
-ulTag.style.paddingLeft = "1rem";
-let displayUL = document.querySelector(".displayList");
-console.log(displayUL);
-console.log(ulTag);
-let userText = "";
+let taskTextBox = document.querySelector("#inputTextBox");
+let addButton = document.querySelector(".ct2Col2 input");
+let display = document.querySelector(".displaySection")
 
-inputInfo.addEventListener("input", () =>
-{
-    userText = inputInfo.value;
-})
+addButton.addEventListener("click", createTaskItem)
 
-addTaskButton.addEventListener("click", () =>
-{
-    //debugger
-    for (let i = 0; i < 1; i++)
-    {
-        let checkBox = document.createElement("input");
-        checkBox.classList.add(".d-inline-`block")
-        checkBox.setAttribute("type", "checkbox");
-        // checkBox.style.marginTop = "35px";
-        holdCheckBox.append(checkBox);
-        let addedTask = document.createElement("li");
-        addedTask.style.paddingRight = "10px";
-        addedTask.style.paddingTop = "10px";
-        addedTask.style.paddingBottom = "10px";
-        let removeButton = document.createElement("button");
-        removeButton.style.marginLeft = "20px";
-        removeButton.textContent = "Remove";
+function createTaskItem() {
+    // Adding classes that help with the alignment of elements being made.
+    let task = document.createElement("div");
+    task.classList.add("row");
+    task.classList.add("todoItem");
+    task.classList.add("d-flex");
+    task.classList.add("justify-content-center");
+    task.classList.add("mt-3");
+    
+    //Making the task appear on the page
+    display.append(task);
 
-        if (userText == "") {}
-        else
-        {
-            holdCheckBox.append(checkBox)
-            displayCheckBox.append(holdCheckBox);
-            addedTask.innerText = userText;
-            ulTag.append(addedTask);
-            addedTask.append(removeButton);
-            displayUL.append(ulTag);
-            inputInfo.value = "";
-            userText = "";
+    let checkBoxDiv = document.createElement("div");
+    checkBoxDiv.classList.add("col-1");
+
+    task.append(checkBoxDiv);
+    
+    //Creating Checkbox
+    let checkBoxInput = document.createElement("input");
+    checkBoxInput.type = "checkbox"
+
+    checkBoxDiv.append(checkBoxInput);
+
+    checkBoxInput.addEventListener("input", () => {
+        if(checkBoxInput.checked == true) {
+            pTag.classList.add("strikeThrough");
+        } else {
+            pTag.classList.remove("strikeThrough");
         }
+    })
 
-        removeButton.addEventListener("click", () =>
-        {
-            ulTag.removeChild(addedTask);
-            ulTag.removeChild(removeButton);
-            inputInfo.value = "";
-            userText = "";
-        })
-    }
-})
+    //* CREATING TASK TITLE COLUMN
+    let title = document.createElement("div")
+    title.classList.add("col-6")
+    title.classList.add("col-sm-4")
+
+    task.append(title);
+
+    //* CREATING P Tag as the element to hold the text inside the input box
+    let pTag = document.createElement("p");
+    pTag.innerHTML = taskTextBox.value
+    title.append(pTag);
+
+    //Making Button Div
+    let buttonColumn = document.createElement("div")
+    buttonColumn.classList.add("col-3");
+    buttonColumn.classList.add("col-sm-2");
+    task.append(buttonColumn);
+
+    //Making Remove Button
+    let removeButton = document.createElement("button")
+    removeButton.innerHTML = "Remove"
+    buttonColumn.append(removeButton);
+    
+    removeButton.addEventListener("click", () => {
+        task.remove();
+    })
+
+    //! CLEARING THE TEXT BOX
+    taskTextBox.value = "";
+}
